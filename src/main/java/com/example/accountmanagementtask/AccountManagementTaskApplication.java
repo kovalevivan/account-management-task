c4d547687d155876181cee75f651e722478b8e7a
Ivan Kovalev <kovalev.ivan.a@gmail.com>
2022-08-28 07:47:27 +0300

    Init repo

---
+++ src/main/java/com/example/accountmanagementtask/AccountManagementTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountmanagementtask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountManagementTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountManagementTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/accountmanagementtask/AccountManagementTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.accountmanagementtask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountManagementTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
